b54bd209a77202b348a2afd2854046713cc6d537
Victarrion <leoheston@gmail.com>
2023-08-28 14:20:30 -0600

    uploading progess. Includes working flask that pulls db sql file

---
+++ Resources/demo.js
@@ -0,0 +1,10 @@
+// Get the Roadster endpoint
+
+
+// Fetch the JSON data and console log it
+var polys = 'Final_Obesity_Covid.json'
+d3.json(polys).then(function(data) {
+  console.log(data);
+});
+
+// Get the capsules endpoint
--- app.py
+++ app.py
@@ -1,135 +1,92 @@
 
 
-#################################################
-# Dependencies
-#################################################
-from flask import Flask, jsonify, render_template
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
+
+#SQL related dependancies to import the data
+from sqlalchemy import create_engine, text, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+import pandas as pd
+import psycopg2
+#Flask related dependencies
+import os
+from flask import Flask, render_template, request, url_for, redirect, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
-#################################################
-# Flask Setup
-#################################################
-app = Flask(__name__)
+from sqlalchemy.sql import func
 
-#################################################
-# Database Setup
-#################################################
-engine = create_engine("sqlite:///Resources/songs_complete_db.sqlite")
+# open connection to database using posgress
 
-# reflect an existing database into a new model
-Base = automap_base()
 
-# # reflect the tables
-Base.prepare(engine, reflect=True)
+# Create a base class for declarating class definitions to produce Table objects
+Base = declarative_base()
 
-# #Save reference to the tables
-# coolness = Base.classes.coolness
-# country_attributes = Base.classes.country_attributes
-# fiftygenre = Base.classes.fiftygenre
-# top_50_by_country = Base.classes.top_50_by_country
-# #song_attibutes = Base.classes.song_attibutes
+
+#funtion used to call and import the sql db being used for the project
+def get_db_connection():
+    conn = psycopg2.connect(host='localhost',
+                            database='Project_3',
+                            user='postgres',
+                            password='postgres')
+    return conn
 
 #################################################
 # Flask Routes
 #################################################
 
-#################################################
-# HOME PAGE
-#################################################
+def index():
+   conn = get_db_connection()
+   cur = conn.cursor()
+   cur.execute('SELECT * FROM "Final_Obesity_Covid_data";')
+   data0 = cur.fetchall()
+   cur.execute('SELECT * FROM "Income_data_and_obesity_data";')
+   data1 = cur.fetchall()
+   cur.execute('SELECT * FROM "PovertyByState_clean1_data";')
+   data2 = cur.fetchall()
+   cur.execute('SELECT * FROM "TotalAccessbydistance_obesity_clean1_data";')
+   data3 = cur.fetchall()
+   data0=pd.DataFrame.from_dict(data0)
+   cur.close()
+   conn.close()
+
+   #convert downloading tables into dataframes
+   data0=pd.DataFrame.from_dict(data0)
+   data1=pd.DataFrame.from_dict(data1)
+   data2=pd.DataFrame.from_dict(data2)
+   data3=pd.DataFrame.from_dict(data3)
+   data_all=data0
+   #merge all dataframes together for ease of access
+   
+   data_all=data_all.merge(data1, how="right",on=0)
+   data_all=data_all.merge(data2, how="right",on=0)
+   data_all=data_all.merge(data3, how="right",on=0)
+   data_all=data_all.rename(columns=[])
+   return jsonify(data_all)
+    # return render_template('index_.html', data = data)
+
+
+
+# Below are the lines of coded we are using to represent the data. Our data is being subdivided in two categories, health and economy.
+# The home or main page will have a drop down menu to allow for a zoomed in version of the data at a per state level but for the sake of 
+# our analysis, we can to represent all the data at the same time in our to tell our data driven story with all the states being present.
+
+app = Flask(__name__)
+
 @app.route('/')
 def home():
-   return render_template('index.html')
+   data=index()
+   return data
 
-#################################################
-# MAP
-#################################################
-@app.route('/map')
-def map():
-   return render_template('map.html')
+@app.route('/economy')
+def economy():
+   return render_template('economy.html')
 
-#################################################
-# ATTRIBUTE  ENDPOINT
-#################################################
-@app.route('/covid')
+@app.route('/health')
 def covid():
    return render_template('covid.html')
 
 
-# def getAttribute(attribute):
-#     # Create our session (link) from Python to the DB, songs_complete_db.sqlite
-#     session = Session(engine)
-#     #print(attribute)
-#     #use the attribute selected by the user; average the values of that attribute for each country
-#     attribute_query = engine.execute(f'SELECT country, AVG({attribute}) \
-#                                       FROM top_50_by_country \
-#                                       GROUP BY country').fetchall()
-#     session.close()
-#     #print(attribute_query)
-#     pakg = [{"country": r[0], "value": r[1]} for r in attribute_query]
-#     print(pakg)
-#     return jsonify(pakg)
 
-# #################################################
-# # COUNTRY ENDPOINT
-# #################################################
-# @app.route('/countries/<country>')
-# def getCountry(country):
-#     # Create our session (link) from Python to the DB, songs_complete_db.sqlite
-#     session = Session(engine)
-#     print(country)
-    
-#     # use the attribute selected by the user; average the values of that attribute for each country
-#     country_query = engine.execute(f"SELECT danceability, energy, loudness,\
-#                                         speechiness, acousticness, valence, \
-#                                         tempo, duration \
-#                                         FROM country_attributes \
-#                                         WHERE country='{country}'").fetchall()
-#      #Result example: [(0.6467594945040479, 0.5531940893832685, -7.848541433554926, 0.09584869347725926, 0.3262092080760877, 0.4660201540182979, 120.01254117027578, 207.08635555848088)]
-#     #print(len(country_results))
-
-#     if len(country_query) == 0:
-#         session.close()
-#         return jsonify([])
-        
-
-#     else:
-#         country_results = country_query[0]
-
-#         avg_query = engine.execute(f"SELECT AVG(danceability), \
-#                                     AVG(energy), \
-#                                     AVG(loudness), \
-#                                     AVG(speechiness), \
-#                                     AVG(acousticness), \
-#                                     AVG(valence), \
-#                                     AVG(tempo), \
-#                                     AVG(duration) \
-#                                     FROM country_attributes").fetchall()
-#         #Result example: [(0.6793448757328516, 0.6398077829193978, -6.404888131435592, 0.1049760341207325, 0.27439794173374626, 0.5325104695279008, 122.01599088347731, 203.17904368185987)]
-#         session.close()
-#         #turn query results into a list
-#         avg_results = avg_query[0]
-#         #round the avg_results to 2 decimals
-#         rnd_avg_results = [str(round(result, 2)) for result in avg_results]
-
-        
-#         #create keys for the final dictionary
-#         keys = ['Danceability', 'Energy', 'Loudness', 'Speechiness', 'Acousticness', 'Valence', 'Tempo', 'Duration' ]
-
-#         results_list = []
-#         i=0
-
-#         for key in keys: 
-#             dict = {'attribute': key, 'value':country_results[i], 'average' : rnd_avg_results[i] }
-#             results_list.append(dict)
-#             i=i+1
-
-#         print(results_list)
-#         return jsonify(results_list)
-
-
-#Because I'm on a mac, I am setting the port to 8000 to avoid access errors
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+   app.run(port=5000, debug=True)
+
+    
